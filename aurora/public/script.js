f5f18f1e78ce536c8ae95c0c2274da7c182028e1
Jasperthepark <Jasperhung888@gmail.com>
2026-05-09 11:42:23 -0600

    upadte

--- aurora/public/script.js
+++ aurora/public/script.js
@@ -3,15 +3,17 @@ allownotfi = document.getElementById('allownotfi')
 allownotfi.onclick = async function(){
     if(Notification.permission=='granted'){
         console.log('permission has already been granted')
+        new Notification('Notifications have already been granted')
+        return
     }else{
-        if(!(Notification in window)){
+        if(!("Notification" in window)){
             console.log("this browser doesn't support permissions")
             new Notification('Notifications have already been enabled')
         }else{
-            Notification.requestPermission().then(()=>{
-                if(Notification.permission=='granted'){
+            Notification.requestPermission().then((permission)=>{
+                if(permission=='granted'){
                     console.log('access grnated!')
-                    new Notification('Notifications have been enabled')
+                    new Notification("Notifications have been enabled")
                 }else{
                     console.log('acess denied ?')
                 }
